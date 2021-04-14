b17c275b895861fba95d7d169029761da335c1cf
Rob Emanuele <rdemanuele@gmail.com>
2021-04-13 21:17:19 -0400

    Change pc.sign to return a url; add pc.sign_link to get SignedLink

--- tests/test_sign_assets.py
+++ tests/test_sign_assets.py
@@ -3,6 +3,8 @@ import json
 import unittest
 from urllib.parse import parse_qs, urlparse
 
+import requests
+
 import planetary_computer as pc
 from planetary_computer.utils import parse_blob_url
 import pystac
@@ -15,6 +17,12 @@ EXP_IMAGE = f"https://{ACCOUNT_NAME}.blob.core.windows.net/{CONTAINER_NAME}/01.t
 EXP_METADATA = f"https://{ACCOUNT_NAME}.blob.core.windows.net/{CONTAINER_NAME}/01.txt"
 EXP_THUMBNAIL = f"https://{ACCOUNT_NAME}.blob.core.windows.net/{CONTAINER_NAME}/01.jpg"
 
+SENTINEL_THUMBNAIL = (
+    "https://sentinel2l2a01.blob.core.windows.net/sentinel2-l2/10/T/ET/2020/10/02/"
+    "S2B_MSIL2A_20201002T191229_N0212_R056_T10TET_20201004T193349.SAFE"
+    "/GRANULE/L2A_T10TET_A018672_20201002T192031/QI_DATA/T10TET_20201002T191229_PVI.tif"
+)
+
 
 def get_sample_item() -> pystac.Item:
     file_path = os.path.abspath(
@@ -26,10 +34,10 @@ def get_sample_item() -> pystac.Item:
 
 
 class TestSignAssests(unittest.TestCase):
-    def assertSignedUrl(self, signed_url: str) -> None:
+    def assertSigned(self, url: str) -> None:
         # Ensure the signed item has an "se" URL parameter added to it,
         # which indicates it has been signed
-        parsed_url = urlparse(signed_url)
+        parsed_url = urlparse(url)
         query_params = parse_qs(parsed_url.query)
         self.assertIsNotNone(query_params["se"])
 
@@ -39,7 +47,7 @@ class TestSignAssests(unittest.TestCase):
         self.assertEqual(CONTAINER_NAME, container)
 
     def test_signed_url(self) -> None:
-        self.assertSignedUrl(pc.sign(EXP_IMAGE).href)
+        self.assertSigned(pc.sign(EXP_IMAGE))
 
     def test_unsigned_assets(self) -> None:
         item = get_sample_item()
@@ -57,4 +65,9 @@ class TestSignAssests(unittest.TestCase):
         for key in ["image", "metadata", "thumbnail"]:
             signed_url = signed_item.assets[key].href
             self.assertNotEqual(unsigned_item.assets[key].href, signed_url)
-            self.assertSignedUrl(signed_url)
+            self.assertSigned(signed_url)
+
+    def test_read_signed_asset(self) -> None:
+        signed_href = pc.sign(SENTINEL_THUMBNAIL)
+        r = requests.get(signed_href)
+        self.assertEqual(r.status_code, 200)
