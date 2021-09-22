6e43de8c534f4082d7f3a80a048c2a08999b0b0e
Tom Augspurger <taugspurger@microsoft.com>
2021-09-22 11:36:44 -0500

    update doc

--- planetary_computer/utils.py
+++ planetary_computer/utils.py
@@ -52,7 +52,7 @@ def is_fsspec_asset(asset: pystac.Asset) -> bool:
     Determine if an Asset points to an fsspec URL.
 
     This checks if "account_name" is present in the asset's "table:storage_options"
-    field.
+    or "xarray:storage_options" fields.
     """
     return "account_name" in asset.extra_fields.get(
         "table:storage_options", {}
